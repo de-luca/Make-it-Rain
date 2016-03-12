55c8ff835c8e49dc18762d93e4e0dc4241e2c08b
Bastien de Luca <dev@de-luca.io>
2016-03-12 15:39:20 +0100

    New menu
    Changed menu.js so it’s a real thing.
    New menu add and support OSX.

--- menu.js
+++ menu.js
@@ -1,6 +1,9 @@
 'use strict';
 
-app.once('ready', function() {
+const app = require('app');
+const Menu = require('menu');
+
+var build = () => {
   var template = [
     {
       label: 'Edit',
@@ -9,41 +12,41 @@ app.once('ready', function() {
           label: 'Undo',
           accelerator: 'CmdOrCtrl+Z',
           role: 'undo'
-        },
-        {
+        }, {
           label: 'Redo',
           accelerator: 'Shift+CmdOrCtrl+Z',
           role: 'redo'
-        },
-        {
+        }, {
           type: 'separator'
-        },
-        {
+        }, {
           label: 'Cut',
           accelerator: 'CmdOrCtrl+X',
           role: 'cut'
-        },
-        {
+        }, {
           label: 'Copy',
           accelerator: 'CmdOrCtrl+C',
           role: 'copy'
-        },
-        {
+        }, {
           label: 'Paste',
           accelerator: 'CmdOrCtrl+V',
           role: 'paste'
-        },
-        {
+        }, {
           label: 'Select All',
           accelerator: 'CmdOrCtrl+A',
           role: 'selectall'
         },
       ]
-    },
-    {
+    }, {
       label: 'View',
       submenu: [
         {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        }, {
           label: 'Toggle Full Screen',
           accelerator: (function() {
             if (process.platform == 'darwin')
@@ -55,10 +58,21 @@ app.once('ready', function() {
             if (focusedWindow)
               focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
           }
+        }, {
+          label: 'Toggle Developer Tools',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Alt+Command+I';
+            else
+              return 'Ctrl+Shift+I';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+          }
         }
       ]
-    },
-    {
+    }, {
       label: 'Window',
       role: 'window',
       submenu: [
@@ -66,31 +80,28 @@ app.once('ready', function() {
           label: 'Minimize',
           accelerator: 'CmdOrCtrl+M',
           role: 'minimize'
-        },
-        {
+        }, {
           label: 'Close',
           accelerator: 'CmdOrCtrl+W',
           role: 'close'
-        },
+        }
       ]
-    },
-    {
+    }, {
       label: 'Help',
       role: 'help',
       submenu: [
         {
           label: 'Learn More',
-          click: function() { shell.openExternal('https://github.com/de-luca/Make-it-Rain'); }
-        },
-        {
+          click: function() { require('shell').openExternal('https://github.com/de-luca/Make-it-Rain'); }
+        }, {
           label: 'Developer website',
-          click: function() { shell.openExternal('http://de-luca.io'); }
+          click: function() { require('shell').openExternal('http://de-luca.io'); }
         }
       ]
-    },
+    }
   ];
 
-  if (process.platform == 'darwin') {
+  if (process.platform === 'darwin') {
     var name = app.getName();
     template.unshift({
       label: name,
@@ -98,53 +109,46 @@ app.once('ready', function() {
         {
           label: 'About '+name,
           role: 'about'
-        },
-        {
+        }, {
           type: 'separator'
-        },
-        {
+        }, {
           label: 'Services',
           role: 'services',
           submenu: []
-        },
-        {
+        }, {
           type: 'separator'
-        },
-        {
+        }, {
           label: 'Hide '+name,
           accelerator: 'Command+H',
           role: 'hide'
-        },
-        {
+        }, {
           label: 'Hide Others',
           accelerator: 'Command+Shift+H',
           role: 'hideothers:'
-        },
-        {
+        }, {
           label: 'Show All',
           role: 'unhide:'
-        },
-        {
+        }, {
           type: 'separator'
-        },
-        {
+        }, {
           label: 'Quit',
           accelerator: 'Command+Q',
           click: function() { app.quit(); }
-        },
+        }
       ]
     });
-    template[3].submenu.push(
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Bring All to Front',
-        role: 'front'
-      }
-    );
-  }
 
-  var menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-});
+    template[3].submenu.push({
+      type: 'separator'
+    }, {
+      label: 'Bring All to Front',
+      role: 'front'
+    });
+  }
+  
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+};
+
+module.exports = {
+  'build': build
+};
